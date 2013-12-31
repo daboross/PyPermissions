c0e0f31fa08d7101a8d0f3cb6397a43b46e943a8
Dabo Ross <daboross@daboross.net>
2013-12-31 00:57:21 -0800

    More stuf

--- Permissions.py
+++ Permissions.py
@@ -1,3 +1,14 @@
-class Permissible:
+class PermissionSet:
     def __init__(self):
+        self.permissions = {}
+        self.cache = {}
+
+    def add(self, permission, value=True):
+        self.permissions[permission] = value
+        cache = {}
+
+    def add_batch(self, ):
+        pass
+
+    def has(self, permission):
         pass
