68b56112a02802dd9ca090fac9924479954cea07
Dabo Ross <daboross@daboross.net>
2013-12-31 02:25:38 -0800

    Stuff

--- generate-doc.sh
+++ generate-doc.sh
@@ -8,7 +8,7 @@ fi
 
 doxygen doxygen.properties
 git checkout gh-pages
-find doxygen -mindepth 1 -maxdepth 1 -exec mv {} ./ \;
+find doxygen/html -mindepth 1 -maxdepth 1 -exec mv {} ./ \;
 rm -r doxygen
 git add -A
 git commit
