9e6e9771f8b6dd731e138b7f1281668474db9ad4
Dabo Ross <daboross@daboross.net>
2013-12-31 02:24:41 -0800

    Update generate-doc.sh

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-find . -type f -name '*.py' -exec pydoc -w {} \;
+git add -A
+git commit
+if [[ "$(git log origin/$(git rev-parse --abbrev-ref HEAD)..HEAD --oneline)" ]]; then
+    git push "$@"
+    git fetch origin
+fi
+
+doxygen doxygen.properties
 git checkout gh-pages
+find doxygen -mindepth 1 -maxdepth 1 -exec mv {} ./ \;
+rm -r doxygen
 git add -A
 git commit
 git push
 git checkout master
-find . -type f -name '*.html' -exec rm -i {} \;
